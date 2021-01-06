8c3041f74254a60e3008b1d65ce1c81c60645d8c
snowbalzz <alek0007@hz.nl>
2021-01-06 12:27:36 +0100

    Added buttons for the setup of the icons.

--- src/core/gui.ts
+++ src/core/gui.ts
@@ -6,19 +6,39 @@ const GAME: GameState = State();
 
 // Elements
 const newPost = document.getElementById('newPost');
+
 const overlay = document.getElementById('overlay');
+
 const modal = document.getElementById('modal');
+
 const menu = document.getElementById(`menu`);
+
+const postBut = document.getElementById('post')
 
 const pause = document.getElementById(`pauseMenu`);
 
+const gameButt = document.getElementById('game');
+let gameButtPress = false;
+
+const bookButt = document.getElementById('book');
+let bookButtPress = false;
+
+
+
+
 // UI
 newPost.addEventListener('click', () => {
-	GAME.relevance += 1;
+	// GAME.relevance += 1;
 	modal.style.top = '45%';
 	overlay.style.opacity = '1';
 	overlay.style.pointerEvents = 'auto';
-	GAME.element.style.transform = 'scale(2) translateY(-32px)';
+	// GAME.element.style.transform = 'scale(2) translateY(-32px)';
+});
+
+postBut.addEventListener ('click', () => {
+	modal.style.top = '150%';
+	overlay.style.opacity = '0';
+	overlay.style.pointerEvents = 'none';
 });
 
 overlay.addEventListener('click', () => {
@@ -37,4 +57,25 @@ pause.addEventListener('click', () => {
 	overlay.style.pointerEvents = 'auto';
 });
 
+//Buttons for the new post thingy
+gameButt.addEventListener('click', () => {
+	if (!gameButtPress) {
+		gameButt.style.backgroundColor = "grey";
+		gameButtPress = true;
+	} else {
+		gameButt.style.backgroundColor = "white";	
+		gameButtPress = false;
+	}
+
+});
+
+bookButt.addEventListener('click', () => {
+	if (!bookButtPress) {
+		bookButt.style.backgroundColor = "grey";
+		bookButtPress = true;
+	} else {
+		bookButt.style.backgroundColor = "white";	
+		bookButtPress = false;
+	}
+});
 
--- src/core/timers.ts
+++ src/core/timers.ts
@@ -145,3 +145,4 @@ function nextNewsBlock() {
 
 // Initialize news
 initNewsBlocks();
+		
